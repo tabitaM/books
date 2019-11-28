a85d3ec713e36e474188013e4ec95550ff4aeef4
Tabita <taby_marcu@yahoo.com>
2019-11-28 16:06:21 +0200

    Implementing Profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -9,10 +9,12 @@ import { IUser } from '../interfaces/IUser';
 })
 export class ProfileComponent {
   private user: IUser = null;
+  private shortDate: Date;
   constructor(private endpointService: EndpointService) {
     this.endpointService.getUser().subscribe((user: IUser) => {
       console.log('User details: ', user);
       this.user = user;
+      this.shortDate = this.user.return_date;
     });
   }
 }
