2d46af493e097d899d58dacf89ce8c01542bbb1e
Marcu Tabita <tabita.marcu@nokia.com>
2020-04-23 13:16:24 +0300

    Implemented filtering by category

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { EndpointService } from '../service/endpoint.service';
+import { IBooks } from '../interfaces/IBooks';
+import { ICategories } from '../interfaces/ICategories';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +9,28 @@ import { EndpointService } from '../service/endpoint.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  booksSelectedByCategory: IBooks[]= [];
+  categorySelectedId: number;
+  isLoaded: boolean = false;
   constructor(private endpointService: EndpointService) {
-    console.log(endpointService);
+    if(!this.isLoaded) {
+      this.booksSelectedByCategory = this.endpointService.books;
+      this.isLoaded = true;
+    }
+    console.log("LOADED: ", this.isLoaded);
+    console.log("BOOKS:", this.booksSelectedByCategory);
+  }
+
+  showBooksByCategory(categorySelected: string) {
+    this.endpointService.categories.filter(category => {
+      if(category.name === categorySelected) {
+        this.categorySelectedId = category.id;
+      }})
+    return this.booksSelectedByCategory = this.endpointService.books.filter(book => book.categoryId === this.categorySelectedId);
+  }
+
+  showAllBooks(): IBooks[] {
+    this.booksSelectedByCategory = this.endpointService.books;
+    return this.booksSelectedByCategory;
   }
 }
